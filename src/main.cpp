c5eaae111aa6e7bb321acc62d42d92fb152cf43e
jaysinco <jaysinco@163.com>
2021-07-26 12:57:58 +0800

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "utils.h"
+
+int main(int argc, char **argv)
+{
+    FLAGS_logtostderr = 1;
+    FLAGS_minloglevel = 0;
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+
+    LOG(INFO) << "hello world!";
+}
