dc9f7094928713328271610e7d91f458f82b598a
jaysinco <jaysinco@163.com>
2021-09-26 16:46:54 +0800

    deep: add conv layer

--- src/deep/fashion-mnist.cpp
+++ src/deep/fashion-mnist.cpp
@@ -122,21 +122,31 @@ const std::map<unsigned, std::string> FashionMnistDataset::labelDescMap = {
 
 struct Net: torch::nn::Module
 {
-    Net(): fc1(784, 256), fc2(256, 10)
+    Net()
+        : conv1(torch::nn::Conv2dOptions(1, 10, 5)),
+          conv2(torch::nn::Conv2dOptions(10, 20, 5)),
+          fc1(320, 50),
+          fc2(50, 10)
     {
+        register_module("conv1", conv1);
+        register_module("conv2", conv2);
         register_module("fc1", fc1);
         register_module("fc2", fc2);
     }
 
     torch::Tensor forward(torch::Tensor x)
     {
-        x = x.reshape({-1, 784});
+        x = torch::relu(torch::max_pool2d(conv1->forward(x), 2));
+        x = torch::relu(torch::max_pool2d(conv2->forward(x), 2));
+        x = x.reshape({-1, 320});
         x = torch::relu(fc1->forward(x));
         x = torch::dropout(x, 0.5, is_training());
         x = fc2->forward(x);
         return torch::log_softmax(x, 1);
     }
 
+    torch::nn::Conv2d conv1;
+    torch::nn::Conv2d conv2;
     torch::nn::Linear fc1;
     torch::nn::Linear fc2;
 };
--- src/deep/main.cpp
+++ src/deep/main.cpp
@@ -1,6 +1,9 @@
 #include "prec.h"
 #include "../utils.h"
 
+DEFINE_bool(lr, false, "linear regression");
+DEFINE_bool(fm, false, "fashion mnist");
+
 void linear_regression();
 void fashion_mnist();
 
@@ -10,11 +13,17 @@ int main(int argc, char **argv)
     FLAGS_minloglevel = 0;
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     google::InitGoogleLogging(argv[0]);
+    google::SetUsageMessage("[[ flags ]]");
 
     TRY_;
     torch::manual_seed(1);
-    // linear_regression();
-    fashion_mnist();
+    if (FLAGS_lr) {
+        linear_regression();
+    } else if (FLAGS_fm) {
+        fashion_mnist();
+    } else {
+        google::ShowUsageWithFlagsRestrict(argv[0], "deep");
+    }
     CATCH_;
     return 0;
 }
