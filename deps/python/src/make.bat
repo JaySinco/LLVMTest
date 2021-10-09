177d7ea3acb90c55db9af65a8dcee7f614e8418e
jaysinco <jaysinco@163.com>
2021-10-09 22:59:19 +0800

    deps: fix python build

--- deps/python/src/make.bat
+++ deps/python/src/make.bat
@@ -10,23 +10,15 @@ IF NOT EXIST ..\..\include (
 )
 IF NOT EXIST ..\..\lib (XCOPY /E/Y/I/F .\PCbuild\amd64\python38.lib ..\..\lib\)
 IF NOT EXIST ..\..\bin (
-    XCOPY /E/Y/I/F .\PCbuild\amd64\*.pyd ..\..\bin\DLLs\
-    XCOPY /E/Y/I/F .\PCbuild\amd64\*.dll ..\..\bin\DLLs\
+    XCOPY /E/Y/I/F .\PCbuild\amd64\*.pyd ..\..\bin\Lib\
+    XCOPY /E/Y/I/F .\PCbuild\amd64\*.dll ..\..\bin\Lib\
     XCOPY /E/Y/I/F .\PCbuild\amd64\python.exe ..\..\bin\
-    DEL /Q ..\..\bin\DLLs\vcruntime140*.dll
-    MOVE ..\..\bin\DLLs\python3*.dll ..\..\bin\
+    DEL /Q ..\..\bin\Lib\vcruntime140*.dll
+    MOVE ..\..\bin\Lib\python3*.dll ..\..\bin\
+    7z a ..\..\bin\Lib\python38.zip .\Lib\*
 )
 
-IF NOT EXIST ..\..\bin\Lib (
-    PUSHD .\Lib
-    MKDIR ..\..\..\bin\Lib\
-    zip -r ..\..\..\bin\Lib\python38.zip .\*
-    POPD
-)
-
-echo .\DLLs> ..\..\bin\python38._pth
+echo .\Lib> ..\..\bin\python38._pth
 echo .\Lib\python38.zip>> ..\..\bin\python38._pth
 echo .>> ..\..\bin\python38._pth
 echo import site>> ..\..\bin\python38._pth
-
-@REM download %~dp0..\bin\Lib\get-pip.py
