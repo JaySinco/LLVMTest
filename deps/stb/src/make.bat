f4e1ba522a9b75c906a82336adb45cf9608a0d1f
jaysinco <jaysinco@163.com>
2021-10-16 00:16:03 +0800

    vcpkg: always use release library

--- deps/stb/src/make.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-PUSHD stb-3a11740
-IF NOT EXIST ..\..\include (XCOPY /Y/I/F .\*.h ..\..\include)
--- deps/wil/src/make.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-PUSHD wil-492c01b
-IF NOT EXIST ..\..\include (XCOPY /E/Y/I/F .\include ..\..\include)
