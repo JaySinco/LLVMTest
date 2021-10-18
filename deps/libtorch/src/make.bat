4e2abaaf8421675d4438af24bc74ea48f4b6da0c
jaysinco <jaysinco@163.com>
2021-10-18 15:54:03 +0800

    vcpkg: use libtorch-ce

--- deps/libtorch/src/make.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-PUSHD libtorch-1.9.0
-IF NOT EXIST ..\..\include (MOVE include ..\..\include)
-IF NOT EXIST ..\..\bin (MOVE bin ..\..\bin)
-IF NOT EXIST ..\..\lib (MOVE lib ..\..\lib)
-IF NOT EXIST ..\..\share (MOVE share ..\..\share)
