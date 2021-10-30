8d15257ad2e82c093611ddfae86deeecb621d4e2
jaysinco <jaysinco@163.com>
2021-10-30 18:43:23 +0800

    vcpkg: add imgui

--- src/gym/env/env.cpp
+++ src/gym/env/env.cpp
@@ -1,5 +1,8 @@
 #include "env.h"
 #include <functional>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 namespace env
 {
@@ -185,6 +188,13 @@ void Env::render()
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+    (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
     mjv_defaultCamera(&cam);
     mjv_defaultOption(&opt);
     mjv_defaultScene(&scn);
@@ -228,10 +238,43 @@ void Env::render()
         mjrRect viewport = {0, 0, 0, 0};
         glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
         mjr_render(viewport, &scn, &con);
-        if (scores.size() > 0) {
-            mjrRect figview = {0, 0, viewport.width / 3, viewport.height / 3};
-            mjr_figure(figview, &figscore, &con);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin(
+                "Hello, world!");  // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");  // Display some text (you can use a format
+            // strings too)
+            bool show_demo_window = true;
+            bool show_another_window = false;
+            ImGui::Checkbox("Demo Window",
+                            &show_demo_window);  // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+            ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+            ImGui::SliderFloat("float", &f, 0.0f,
+                               1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color",
+                              (float *)&clear_color);  // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))  // Buttons return true when clicked (most widgets return
+                                          // true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                        1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
         }
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
     }
 
