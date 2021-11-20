4a9bef9a3026b23d185a607a29160421c79c13fa
jaysinco <jaysinco@163.com>
2021-11-20 23:25:07 +0800

    parser: add completion hook

--- src/parser/testing.cpp
+++ src/parser/testing.cpp
@@ -2,6 +2,7 @@
 #include ".antlr/lexers.h"
 #include ".antlr/parsers.h"
 #include "replxx.hxx"
+#include <optional>
 
 class ErrorListener: public antlr4::BaseErrorListener
 {
@@ -45,8 +46,61 @@ bool eval(const std::string &code)
     }
 }
 
-replxx::Replxx::completions_t completion(const std::string &context, int &contextLen,
-                                         const replxx::Replxx &rx)
+std::optional<antlr4::tree::ParseTree *> getTreeFromPos(antlr4::tree::ParseTree *root,
+                                                        size_t col /* 0..n-1 */,
+                                                        size_t row /* 1..n   */)
+{
+    if (auto node = dynamic_cast<antlr4::tree::TerminalNode *>(root)) {
+        auto tok = node->getSymbol();
+        if (tok->getLine() != row) {
+            return {};
+        }
+        if (auto last = tok->getCharPositionInLine() + tok->getStopIndex() - tok->getStartIndex();
+            tok->getCharPositionInLine() <= col && last >= col) {
+            return node;
+        }
+        return {};
+    } else {
+        auto ctx = dynamic_cast<antlr4::ParserRuleContext *>(root);
+        auto start = ctx->getStart();
+        auto stop = ctx->getStop();
+        if (start == nullptr || stop == nullptr) {
+            return {};
+        }
+        if (start->getLine() > row ||
+            (start->getLine() == row && col < start->getCharPositionInLine())) {
+            return {};
+        }
+        auto last = stop->getCharPositionInLine() + stop->getStopIndex() - stop->getStartIndex();
+        if (stop->getLine() < row || (stop->getLine() == row && last < col)) {
+            return {};
+        }
+        for (auto child: ctx->children) {
+            if (auto target = getTreeFromPos(child, col, row)) {
+                return target;
+            }
+        }
+        return ctx;
+    }
+}
+
+std::optional<std::string> completion(antlr4::Parser *parser, antlr4::tree::ParseTree *caret)
+{
+    if (auto node = dynamic_cast<antlr4::tree::TerminalNode *>(caret)) {
+        auto type = node->getSymbol()->getType();
+        if (type == parser::lexers::Dot) {
+            if (auto ctx =
+                    dynamic_cast<parser::parsers::MemberDotExpressionContext *>(node->parent)) {
+                return fmt::format("<MemberDot> completion based on typeof => \n{}",
+                                   ctx->singleExpression()->toStringTree(parser, true));
+            }
+        }
+    }
+    return {};
+}
+
+replxx::Replxx::completions_t hook_completion(const std::string &context, int &contextLen,
+                                              const replxx::Replxx &rx)
 {
     replxx::Replxx::completions_t completions;
     antlr4::ANTLRInputStream ais(rx.get_state().text());
@@ -59,7 +113,14 @@ replxx::Replxx::completions_t completion(const std::string &context, int &contex
     parsing.removeErrorListeners();
     parsing.addErrorListener(&lerr);
     auto tree = parsing.singleExpression();
-    int curPos = rx.get_state().cursor_position();
+    if (int pos = rx.get_state().cursor_position(); pos != 0) {
+        if (auto caret = getTreeFromPos(tree, pos - 1, 1)) {
+            if (auto pl = completion(&parsing, *caret)) {
+                completions.push_back(*pl);
+                completions.push_back("");
+            }
+        }
+    }
     return completions;
 }
 
@@ -67,7 +128,7 @@ int main(int argc, char **argv)
 {
     using namespace std::placeholders;
     replxx::Replxx rx;
-    rx.set_completion_callback(std::bind(&completion, _1, _2, std::cref(rx)));
+    rx.set_completion_callback(std::bind(&hook_completion, _1, _2, std::cref(rx)));
 
     while (true) {
         const char *text = nullptr;
