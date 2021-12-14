a89de1b184a43853bf51b2005670d0f0ac1fe7f8
jaysinco <jaysinco@163.com>
2021-12-14 17:36:45 +0800

    vcpkg: use versioning

--- make.bat
+++ make.bat
@@ -2,6 +2,7 @@
 
 SET OUTDIR=out
 SET BINDIR=bin
+SET LLVMBIN=C:/Program Files/LLVM/bin
 
 IF "%1" == "clean" (
     IF EXIST %OUTDIR% (RMDIR /S/Q %OUTDIR%)
@@ -13,15 +14,15 @@ PUSHD C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 CALL VC\Auxiliary\Build\vcvars64.bat
 POPD
 
-@REM FOR /R %~dp0src %%f IN (*.h *.cpp) DO ("C:/Program Files/LLVM/bin/clang-format.exe" -i %%f)
+FOR /R %~dp0src %%f IN (*.h *.cpp) DO ("%LLVMBIN%/clang-format.exe" -i %%f)
 
 IF NOT EXIST %OUTDIR% (MKDIR %OUTDIR%)
 PUSHD %OUTDIR%
 cmake -G Ninja ^
     -DVCPKG_ROOT_DIR=%~dp0deps\vcpkg ^
-    -DCMAKE_C_COMPILER="C:/Program Files/LLVM/bin/clang-cl.exe" ^
-    -DCMAKE_CXX_COMPILER="C:/Program Files/LLVM/bin/clang-cl.exe" ^
-    -DCMAKE_LINKER="C:/Program Files/LLVM/bin/lld-link.exe" ^
+    -DCMAKE_C_COMPILER="%LLVMBIN%/clang-cl.exe" ^
+    -DCMAKE_CXX_COMPILER="%LLVMBIN%/clang-cl.exe" ^
+    -DCMAKE_LINKER="%LLVMBIN%/lld-link.exe" ^
     -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=%~dp0%BINDIR%\ ^
     -DCMAKE_BUILD_TYPE=Debug ^
     ..
