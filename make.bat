ea7c9c7e9f18c68c58095467b17d09bbfdeab359
jaysinco <jaysinco@163.com>
2022-01-06 13:10:12 +0800

    make: change vars to small case

--- make.bat
+++ make.bat
@@ -11,36 +11,34 @@ echo.
 goto end
 
 :run
-set DIR=%~dp0
-set OUTDIR=out
-set BINDIR=bin
-set LLVMBIN=C:/Program Files/LLVM/bin
-set CMAKECACHE=%OUTDIR%\CMakeCache.txt
-set MANIFEST=off
+set dir=%~dp0
+set outdir=out
+set bindir=bin
+set llvmbin=C:/Program Files/LLVM/bin
+set cmakecache=%outdir%\CMakeCache.txt
+set manifest=off
 
 :flags
 if "%~1"=="-h" goto usage
-if "%~1"=="-d" if exist %CMAKECACHE% del /q/s %CMAKECACHE% & shift & goto flags
-if "%~1"=="-m" (set MANIFEST=on) & shift & goto flags
+if "%~1"=="-d" if exist %cmakecache% del /q/s %cmakecache% & shift & goto flags
+if "%~1"=="-m" (set manifest=on) & shift & goto flags
 
-pushd C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
-call VC\Auxiliary\Build\vcvars64.bat
-popd
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-for /r %DIR%src %%f in (*.h *.cpp) do "%LLVMBIN%/clang-format.exe" -i %%f
+for /r %dir%src %%f in (*.h *.cpp) do "%llvmbin%/clang-format.exe" -i %%f
 
-if not exist %OUTDIR% mkdir %OUTDIR%
-pushd %OUTDIR%
+if not exist %outdir% mkdir %outdir%
+pushd %outdir%
 cmake -G Ninja ^
-    -DVCPKG_ROOT_DIR=%DIR%deps\vcpkg ^
-    -DVCPKG_MANIFEST_MODE=%MANIFEST% ^
-    -DCMAKE_C_COMPILER="%LLVMBIN%/clang-cl.exe" ^
-    -DCMAKE_CXX_COMPILER="%LLVMBIN%/clang-cl.exe" ^
-    -DCMAKE_LINKER="%LLVMBIN%/lld-link.exe" ^
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=%DIR%%BINDIR%\ ^
+    -DVCPKG_ROOT_DIR=%dir%deps\vcpkg ^
+    -DVCPKG_MANIFEST_MODE=%manifest% ^
+    -DCMAKE_C_COMPILER="%llvmbin%/clang-cl.exe" ^
+    -DCMAKE_CXX_COMPILER="%llvmbin%/clang-cl.exe" ^
+    -DCMAKE_LINKER="%llvmbin%/lld-link.exe" ^
+    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=%dir%%bindir%\ ^
     -DCMAKE_BUILD_TYPE=Debug ^
     ..
-if %ERRORLEVEL% == 0 (ninja %1)
+if %errorlevel% == 0 (ninja %1)
 popd
 
 :end
