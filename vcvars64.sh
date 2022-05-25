d7173fc5a1c97a44504d51a68e5863dbb163615a
jaysinco <jaysinco@163.com>
2022-05-25 23:01:31 +0800

    vcvars: upgrade msvc version

--- vcvars64.sh
+++ vcvars64.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PATH="\
-/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30037/bin/HostX64/x64:\
+/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.29.30133/bin/HostX64/x64:\
 /c/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/VC/VCPackages:\
 /c/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/TestWindow:\
 /c/Program Files (x86)/Microsoft Visual Studio/2019/Community/Common7/IDE/CommonExtensions/Microsoft/TeamFoundation/Team Explorer:\
@@ -20,8 +20,8 @@ export PATH="\
 $PATH"
 
 export INCLUDE="\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\ATLMFC\include;\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\include;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\ATLMFC\include;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\include;\
 C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\ucrt;\
 C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\shared;\
 C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\um;\
@@ -29,15 +29,15 @@ C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\winrt;\
 C:\Program Files (x86)\Windows Kits\10\include\10.0.19041.0\cppwinrt"
 
 export LIB="\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\ATLMFC\lib\x64;\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\lib\x64;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\ATLMFC\lib\x64;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\lib\x64;\
 C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\ucrt\x64;\
 C:\Program Files (x86)\Windows Kits\10\lib\10.0.19041.0\um\x64"
 
 export LIBPATH="\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\ATLMFC\lib\x64;\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\lib\x64;\
-C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\lib\x86\store\references;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\ATLMFC\lib\x64;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\lib\x64;\
+C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133\lib\x86\store\references;\
 C:\Program Files (x86)\Windows Kits\10\UnionMetadata\10.0.19041.0;\
 C:\Program Files (x86)\Windows Kits\10\References\10.0.19041.0;\
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319"
