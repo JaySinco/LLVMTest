e1a47caba3a3b6cc744a29d62ece7c9fe166a5ad
jaysinco <jaysinco@163.com>
2022-07-18 22:33:56 +0800

    qt5: don't use new

--- src/qt5/testing.cpp
+++ src/qt5/testing.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
-    auto* window = new GoToCellDialog;
-    window->show();
+    GoToCellDialog w;
+    w.show();
     return app.exec();
 }
