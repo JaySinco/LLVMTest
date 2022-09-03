4a21e9368a97a3965f9d23ec256358b6671b06dc
jaysinco <jaysinco@163.com>
2022-09-03 22:33:26 +0800

    refactor build script

--- build.sh
+++ build.sh
@@ -2,112 +2,87 @@
 
 set -e
 
-case "$(uname -m)" in
-    x86_64)   ARCH=x64 ;;
-esac
+build_release=0
 
-case "$OSTYPE" in
-    linux*)   PLATFORM=linux ;;
-    msys*)    PLATFORM=win32 ;;
-esac
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-DOCKER_WORKSPACE_DIR=/workspace
-DOCKER_IMAGE_TAG=build:v1
-BUILD_TYPE=Debug
-BUILD_TARGETS=()
-CONAN_PROFILE="$PROJECT_ROOT/config/$PLATFORM/$ARCH/conan.profile"
-
-if [ $PLATFORM = "linux" ]; then
-    BUILD_C_COMPILER="clang"
-    BUILD_CXX_COMPILER="clang++"
-elif [ $PLATFORM = "win32" ]; then
-    BUILD_C_COMPILER="cl"
-    BUILD_CXX_COMPILER="cl"
-    source $PROJECT_ROOT/vcvars64.sh
-fi
+do_clean=0
+do_run_docker=0
+do_rm_cmake_cache=0
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        -h|--help)
+        -h)
             echo
-            echo "Usage: build.sh [options] [targets]"
+            echo "Usage: build.sh [options]"
             echo
             echo "Options:"
-            echo "  -r, --docker-run      run dev container"
-            echo "  -u, --edit-ui         edit qt ui files"
-            echo "  -q, --qt-doc          open qt assistant"
-            echo "  -c, --clean           clean targets built"
-            echo "      --release         build release version"
-            echo "  -f, --force-config    remove cmake cache before build"
-            echo "  -h, --help            print command line options"
+            echo "  -c   clean output"
+            echo "  -r   run docker"
+            echo "  -e   build release version"
+            echo "  -f   remove cmake cache before build"
+            echo "  -h   print command line options"
             echo
             exit 0
             ;;
-        -r|--docker-run)
-            docker run -it --rm \
-                -v $HOME/.ssh:/root/.ssh:ro \
-                -v $HOME/.config/nvim:/root/.config/nvim:rw \
-                -v $HOME/.local/share/nvim:/root/.local/share/nvim:rw \
-                -v $PROJECT_ROOT:$DOCKER_WORKSPACE_DIR:rw \
-                $DOCKER_IMAGE_TAG
-            exit 0
-            ;;
-        -u|--edit-ui)
-            $PROJECT_ROOT/deps/$PLATFORM/qt5/bin/designer \
-                $PROJECT_ROOT/src/qt5/*.ui &
-            exit 0
-            ;;
-        -q|--qt-doc)
-            $PROJECT_ROOT/deps/$PLATFORM/qt5/bin/assistant &
-            exit 0
-            ;;
-        --release)
-            BUILD_TYPE=release
-            shift
-            ;;
-        -f|--force-config)
-            rm -f $PROJECT_ROOT/out/CMakeCache.txt
-            shift
-            ;;
-        -c|--clean)
-            pushd $PROJECT_ROOT/out && $BUILD_PROGRAM clean && popd
-            exit 0
-            ;;
-        -*|--*)
-            echo "Unknown option: $1"
-            exit 1
-            ;;
-        *)
-            BUILD_TARGETS+=("$1")
-            shift
-            ;;
+        -c) do_clean=1 && shift ;;
+        -r) do_run_docker=1 && shift ;;
+        -e) build_release=1 && shift ;;
+        -f) do_rm_cmake_cache=1 && shift ;;
+        -*) echo "Unknown option: $1" && exit 1 ;;
     esac
 done
 
-echo "[settings]"
-echo "platform=$PLATFORM"
-echo "build_type=$BUILD_TYPE"
-echo "compiler=$BUILD_CXX_COMPILER"
-echo
+case "$(uname -m)" in
+    x86_64)   arch=x64 ;;
+esac
 
-pushd $PROJECT_ROOT \
+case "$OSTYPE" in
+    linux*)   os=linux ;;
+    msys*)    os=windows ;;
+esac
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+git_root="$(git rev-parse --show-toplevel)"
+
+docker_workspace_dir=/workspace
+docker_image_tag=build:v1
+
+conan_build_type=Debug
+if [ $build_release -eq 1 ]; then
+    conan_build_type=Release
+fi
+
+conan_profile=$git_root/profiles/$arch-$os.profile
+conan_build_folder=$git_root/out/$conan_build_type
+
+if [ $do_clean -eq 1 ]; then
+    cd $conan_build_folder && ninja clean
+    exit 0
+fi
+
+if [ $do_run_docker -eq 1 ]; then
+    docker run -it --rm \
+        -v $HOME/.ssh:/root/.ssh:ro \
+        -v $HOME/.config/nvim:/root/.config/nvim:rw \
+        -v $HOME/.local/share/nvim:/root/.local/share/nvim:rw \
+        -v $git_root:$docker_workspace_dir:rw \
+        $docker_image_tag
+    exit 0
+fi
+
+if [ $do_rm_cmake_cache -eq 1 ]; then
+    rm -f $conan_build_folder/CMakeCache.txt
+fi
+
+pushd $git_root \
 && find src -iname *.h -or -iname *.cpp | xargs clang-format -i \
 && conan install \
-    --profile=$CONAN_PROFILE\
-    --install-folder=$PROJECT_ROOT/out/generators \
+    --install-folder=$conan_build_folder \
+    --profile=$conan_profile \
+    --profile:build=$conan_profile \
+    --conf=tools.cmake.cmaketoolchain:generator=Ninja \
+    --settings=build_type=$conan_build_type \
     --build=never \
     . \
-&& mkdir -p out/$BUILD_TYPE \
-&& pushd out/$BUILD_TYPE \
-&& cmake -G "Ninja" ../.. \
-    -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$PROJECT_ROOT/bin \
-    -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
-    -DCMAKE_C_COMPILER=$BUILD_C_COMPILER \
-    -DCMAKE_CXX_COMPILER=$BUILD_CXX_COMPILER \
-    -DCMAKE_PREFIX_PATH=$PROJECT_ROOT/out/generators \
-    -DTARGET_OS=$PLATFORM \
-&& cp compile_commands.json $PROJECT_ROOT \
-&& ninja -j`nproc` ${BUILD_TARGETS[*]} \
+&& conan build --install-folder=$conan_build_folder . \
+&& cp $conan_build_folder/compile_commands.json $git_root \
 && echo done!
---
+++ conanfile.py
@@ -0,0 +1,59 @@
+from conans import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, CMakeDeps
+import os
+import sys
+
+
+class SpdlogConan(ConanFile):
+    name = "prototyping"
+    version = "0.1.0"
+    url = "https://github.com/JaySinco/conan"
+    homepage = "https://github.com/JaySinco/Prototyping"
+    description = "C++ prototype repo"
+    license = "MIT"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "gflags:nothreads": False,
+    }
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def requirements(self):
+        self.requires("gflags/2.2.2@jaysinco/stable")
+        self.requires("spdlog/1.10.0@jaysinco/stable")
+        self.requires("boost/1.79.0@jaysinco/stable")
+
+    def layout(self):
+        build_folder = "out"
+        build_type = str(self.settings.build_type)
+        self.folders.source = ""
+        self.folders.build = os.path.join(build_folder, build_type)
+        self.folders.generators = os.path.join(
+            self.folders.build, "generators")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CMAKE_RUNTIME_OUTPUT_DIRECTORY"] = os.path.join(
+            self.source_folder, "bin").replace("\\", "/")
+        tc.variables["TARGET_OS"] = sys.platform
+        tc.generate()
+        cmake_deps = CMakeDeps(self)
+        cmake_deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
