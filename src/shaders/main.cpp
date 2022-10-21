9089b85b6970a97f0840e39c4479b86fc2e30f9a
jaysinco <jaysinco@163.com>
2022-10-21 15:20:29 +0800

    add base shader

--- src/shaders/main.cpp
+++ src/shaders/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     int const screenHeight = 450;
 
     SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(screenWidth, screenHeight, "raylib [shaders] example - model shader");
+    InitWindow(screenWidth, screenHeight, "shaders");
 
     Camera camera = {0};
     camera.position = Vector3{4.0f, 4.0f, 4.0f};
@@ -16,11 +16,15 @@ int main(int argc, char** argv)
     camera.fovy = 45.0f;
     camera.projection = CAMERA_PERSPECTIVE;
 
-    Model model = LoadModel((utils::source_repo / "models/watermill.obj").string().c_str());
-    Texture2D texture =
-        LoadTexture((utils::source_repo / "models/watermill_diffuse.png").string().c_str());
+    auto modelPath = utils::source_repo / "models/watermill.obj";
+    Model model = LoadModel(modelPath.string().c_str());
 
-    Shader shader = LoadShader(0, (__DIRNAME__ / "grayscale.fs").string().c_str());
+    auto texturePath = utils::source_repo / "models/watermill_diffuse.png";
+    Texture2D texture = LoadTexture(texturePath.string().c_str());
+
+    auto vertexShaderPath = __DIRNAME__ / "base.vs";
+    auto fragShaderPath = __DIRNAME__ / "grayscale.fs";
+    Shader shader = LoadShader(vertexShaderPath.string().c_str(), fragShaderPath.string().c_str());
 
     model.materials[0].shader = shader;
     model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
@@ -38,8 +42,6 @@ int main(int argc, char** argv)
         DrawModel(model, position, 0.2f, WHITE);
         DrawGrid(10, 1.0f);
         EndMode3D();
-        DrawText("(c) Watermill 3D model by Alberto Cano", screenWidth - 210, screenHeight - 20, 10,
-                 GRAY);
         DrawFPS(10, 10);
         EndDrawing();
     }
