c4f7244b526f6a24b8b73676ba95ad80e5c34e41
jaysinco <jaysinco@163.com>
2023-03-21 23:30:12 +0800

    add some apt mirror

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,7 +3,9 @@
 # apt mirror
 # --------------
 # cp /etc/apt/sources.list /etc/apt/sources.list.bak
-# printf 'deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse\ndeb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse\ndeb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse\ndeb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse\ndeb-src https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse\n' > /etc/apt/sources.list
+# printf 'deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-updates main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-backports main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ focal-security main restricted universe multiverse\ndeb https://launchpad.proxy.ustclug.org/ubuntu-toolchain-r/test/ubuntu focal main\ndeb https://mirrors.tuna.tsinghua.edu.cn/llvm-apt/focal/ llvm-toolchain-focal-15 main\ndeb https://mirrors.ustc.edu.cn/nodesource/deb/node_16.x focal main\n' > /etc/apt/sources.list
+# wget -q -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+# wget -q -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 # apt-get update -y
 
 # ssh key
@@ -18,6 +20,11 @@ set -e
 git_root="$(git rev-parse --show-toplevel)"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source_repo=/mnt/c/Users/jaysinco/OneDrive/src
+
+if ! lsb_release -r | grep -q "20.04"; then
+    echo "ubuntu:20.04 required!"
+    exit 1
+fi
 
 git config --global i18n.filesEncoding utf-8
 git config --global core.autocrlf input
@@ -48,8 +55,13 @@ function clone_repo() {
 clone_repo $git_root jaysinco/Prototyping.git master
 clone_repo $HOME/.config/nvim jaysinco/nvim.git master
 
-if [ ! -f "/usr/bin/ninja" ]; then sudo apt-get install ninja-build; fi
-if [ ! -f "/usr/bin/unzip" ]; then sudo apt-get install unzip; fi
+if [ ! -f "/usr/bin/ninja" ]; then sudo apt-get install -y ninja-build; fi
+if [ ! -f "/usr/bin/unzip" ]; then sudo apt-get install -y unzip; fi
+if [ ! -f "/usr/bin/cmake" ]; then sudo apt-get install -y cmake; fi
+if [ ! -f "/usr/bin/node" ]; then sudo apt-get install -y nodejs; fi
+if [ ! -f "/usr/bin/python3" ]; then sudo apt-get install -y python3 python3-pip; fi
+if [ ! -f "$HOME/.local/bin/conan" ]; then pip3 install conan==1.52 -i https://pypi.tuna.tsinghua.edu.cn/simple; fi
+if [ ! -f "/usr/bin/pyright" ]; then sudo npm install -g pyright; fi
 
 if [ ! -f "/usr/bin/nvim" ]; then
     echo "install nvim"
@@ -61,4 +73,20 @@ if [ ! -d "$nvim_data_dir/site" ]; then
     echo "copy nvim data"
     mkdir -p $nvim_data_dir
     unzip -q $source_repo/nvim-data-site-v0.7.2-linux-x86_64.zip -d $nvim_data_dir
+fi
+
+if [ ! -f "/usr/bin/gcc" ]; then
+    sudo apt-get install -y gcc-11 g++-11
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 \
+        --slave /usr/bin/g++ g++ /usr/bin/g++-11
+fi
+
+if [ ! -f "/usr/bin/clangd" ]; then
+    sudo apt-get install -y clangd-15 clang-format-15 clang-tidy-15 lldb-15 libclang-15-dev
+    sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-15 60 \
+        --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-15 \
+        --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-15 \
+        --slave /usr/bin/lldb lldb /usr/bin/lldb-15 \
+        --slave /usr/bin/lldb-vscode lldb-vscode /usr/bin/lldb-vscode-15 \
+        --slave /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-15
 fi
